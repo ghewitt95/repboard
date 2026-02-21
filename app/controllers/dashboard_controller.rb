70bbfe71a77e17490a66d4c15082f9e6ae4cae9e
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2026-02-21 13:44:48 +0000

    Add Kaminari pagination to freelancer dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @reviews = current_user.reviews_received.order(created_at: :desc)
+    @reviews = current_user.reviews_received.order(created_at: :desc).page(params[:page]).per(10)
   end
 end

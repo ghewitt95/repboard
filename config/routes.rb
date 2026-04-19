dbfd3bfe4009aae8a376bd8f0c66eecbe1fc841c
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2026-04-19 19:02:42 +0000

    Replace hardcoded URLs in layout eith route helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  get "/dashboard" => "dashboard#index"
-  get "/client-dashboard" => "client_dashboard#index"
+  get "/dashboard" => "dashboard#index", as: :dashboard
+  get "/client-dashboard" => "client_dashboard#index", as: :client_dashboard
 
   # Routes for the Review resource:
-  post "/reviews" => "reviews#create"
-  delete "/reviews/:id" => "reviews#destroy"
+  post "/reviews" => "reviews#create", as: :reviews
+  delete "/reviews/:id" => "reviews#destroy", as: :review
 
   # Profile page
-  get "/profile/:slug" => "profile#show"
+  get "/profile/:slug" => "profiles#show", as: :profile
 
   # Settings
-  get "/settings" => "settings#edit"
+  get "/settings" => "settings#edit", as: :settings
   patch "/settings" => "settings#update"
 
   devise_for :users

525c428a8c005d0a82cc4f529609557994262921
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2026-04-21 17:37:01 +0000

    Use strong params and mass assignment in ReviewsController#create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,18 +2,12 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    review_params = params.fetch(:review)
-    reviewee = User.find(review_params.fetch(:reviewee_id))
+    service = CreateReview.new(**review_params.to_h.symbolize_keys, reviewer: current_user)
 
-    service = CreateReview.new(
-      reviewer: current_user,
-      reviewee_id: reviewee.id,
-      body: review_params.fetch(:body),
-      stars: review_params.fetch(:stars)
-    )
     if service.call
       redirect_to profile_path(service.review.reviewee.slug), notice: "Review submitted successfully."
     else
+      reviewee = User.find(review_params[:reviewee_id])
       redirect_to profile_path(reviewee.slug), alert: service.error
     end
   end
@@ -28,5 +22,11 @@ class ReviewsController < ApplicationController
     else
       redirect_to profile_path(the_review.reviewee.slug), alert: "Not authorized."
     end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body, :stars, :reviewee_id)
   end
 end
